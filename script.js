8f85e83c9fc572844a4b96de3f3e0a1eb51c6bde
billpsem <billpsem@gmail.com>
2025-03-18 20:44:52 +0200

    change padding on opera

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const userAgent = navigator.userAgent.toLowerCase();
 const emptySpace = document.querySelector(".empty-space");
 
 if (userAgent.includes("opr") || userAgent.includes("opera")) {
-    emptySpace.style.paddingTop = "230px";
-    emptySpace.style.paddingBottom = "230px"; // Opera GX
+    emptySpace.style.paddingTop = "185px";
+    emptySpace.style.paddingBottom = "185px"; // Opera GX
 } else if (userAgent.includes("edg")) {
     emptySpace.style.paddingTop = "240px";
     emptySpace.style.paddingBottom = "240px"; // Microsoft Edge
