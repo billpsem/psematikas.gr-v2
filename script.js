1a5388028d6b6bba9ebd2b97f7aded1846547065
billpsem <billpsem@gmail.com>
2025-03-20 19:36:39 +0200

    fix emptyspace padding for opera

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const userAgent = navigator.userAgent.toLowerCase();
 const emptySpace = document.querySelector(".empty-space");
 
 if (userAgent.includes("opr") || userAgent.includes("opera")) {
-    emptySpace.style.paddingTop = "185px";
-    emptySpace.style.paddingBottom = "185px"; // Opera GX
+    emptySpace.style.paddingTop = "90px";
+    emptySpace.style.paddingBottom = "90px"; // Opera GX
 } else if (userAgent.includes("edg")) {
     emptySpace.style.paddingTop = "240px";
     emptySpace.style.paddingBottom = "240px"; // Microsoft Edge
