ba32a8bfa5dfd7fcbcd48641fce121ef2d7ea7f4
billpsem <billpsem@gmail.com>
2025-03-20 20:21:05 +0200

    fix edge and chrome version for pc

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ if (userAgent.includes("opr") || userAgent.includes("opera")) {
     emptySpace.style.paddingTop = "90px";
     emptySpace.style.paddingBottom = "90px"; // Opera GX
 } else if (userAgent.includes("edg")) {
-    emptySpace.style.paddingTop = "240px";
-    emptySpace.style.paddingBottom = "240px"; // Microsoft Edge
+    emptySpace.style.paddingTop = "100px";
+    emptySpace.style.paddingBottom = "100px"; // Microsoft Edge
 } else if (userAgent.includes("chrome")) {
-    emptySpace.style.paddingTop = "240px";
-    emptySpace.style.paddingBottom = "240px"; // Google Chrome
+    emptySpace.style.paddingTop = "100px";
+    emptySpace.style.paddingBottom = "100px"; // Google Chrome
 }
